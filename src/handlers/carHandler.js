565cf04a2897be4e91000e550d50a7dff05cdadc
Ebraheem Ghantous <ave.brhom@gmail.com>
2021-01-26 15:17:58 +0200

    fixing issues

--- src/handlers/carHandler.js
+++ src/handlers/carHandler.js
@@ -1,24 +1,39 @@
-const fetchCar = require("./src/carHandlers");
+const fetchCar = require("../fetchData");
 
-function carHandlers(request, response) {
-    fetchCar();
-    let body = request.body;
+function carHandler(request, response) {
+    let body = "";
+    request.on("data", chunk => {
+        body += chunk;
+    })
+    request.on("end", () => {
+        let carsList = [];
+        fetchCar()
+            .then(data => {
+                carsList = [...data]
 
-    dataToReturn = [];
-    for (let i = 0; i < carsList.length; i++) {
-        if (carsList[i].make.includes(body) || carsList[i].model.includes(body)) {
-            dataToReturn.push(carsList[i]);
-        }
-    }
+                let dataToReturn = [];
+                for (let i = 0; i < carsList.length; i++) {
+                    if (carsList[i].make.includes(body) || carsList[i].model.includes(body)) {
+                        dataToReturn.push(carsList[i]);
+                    }
+                }
+                //console.log(dataToReturn);
 
-    if (dataToReturn.length > 0) {
-        response.writeHead(200, { "content-type": "application/json" });
-        response.end(JSON.stringify(carsList));
-    } else {
-        response.writeHead(404, { "content-type": "application/json" });
-        response.end(JSON.stringify(carsList));
+                if (dataToReturn.length > 0) {
+                    response.writeHead(200, { "content-type": "application/json" });
+                    response.end(JSON.stringify(dataToReturn));
+                } else {
+                    response.writeHead(404, { "content-type": "application/json" });
+                    response.end(JSON.stringify(dataToReturn));
 
-    }
+                }
+
+            }).catch(error => console.error(error))
+        console.log(carsList);
+
+
+    })
+
 }
 
-module.exports = carHandlers;
+module.exports = carHandler;
--- src/router.js
+++ src/router.js
@@ -1,18 +1,17 @@
-
 const carHandler = require("./handlers/carHandler");
 const homeHandlers = require("./handlers/homeHandlers");
 const missingHandler = require("./handlers/missingHandler");
 const publicHandler = require("./handlers/publicHandler");
-const router = (req, res) => {
-  const url = request.url;
-  if (url === "/") {
-    homeHandler(request, response);
-  } else if (url.includes("public")) {
-    publicHandler(request, response);
-  } else if (req.url === "/car") {
-    carHandler(request, response);
-  } else {
-    missingHandler(request, response);
-  }
+const router = (request, response) => {
+    const url = request.url;
+    if (url === "/") {
+        homeHandlers(request, response);
+    } else if (url.includes("public")) {
+        publicHandler(request, response);
+    } else if (url === "/car") {
+        carHandler(request, response);
+    } else {
+        missingHandler(request, response);
+    }
 };
 module.exports = router;
