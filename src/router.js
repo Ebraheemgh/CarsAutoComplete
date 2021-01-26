0070bdf05a2719e85670014473f3f6c0488eb64d
Ebraheem Ghantous <ave.brhom@gmail.com>
2021-01-26 12:41:18 +0200

    adding the carHandeler

---
+++ src/router.js
@@ -0,0 +1,101 @@
+const carsList = [{
+        type: "toyota",
+        startingYear: "1937",
+        image: "https://cdn-ds.com/blogs-media/sites/264/2016/10/09090408/toyota_logo_o.jpg"
+    },
+    {
+        type: "bmw",
+        startingYear: "1916",
+        image: "https://images-na.ssl-images-amazon.com/images/I/61HobFlkn-L._AC_SL1040_.jpg"
+    },
+    {
+        type: "ford",
+        startingYear: "1903",
+        image: "https://listcarbrands.com/wp-content/uploads/2016/03/ford-badge.jpg"
+    },
+    {
+        type: "volkswagen",
+        startingYear: "1937",
+        image: "https://images-na.ssl-images-amazon.com/images/I/41sxjEtl8QL._AC_.jpg"
+    },
+    {
+        type: "seat",
+        startingYear: "1950",
+        image: "https://car-logos.net/wp-content/uploads/2018/09/seat-logo.jpg"
+    },
+    {
+        type: "toyota",
+        startingYear: "1937",
+        image: "https://cdn-ds.com/blogs-media/sites/264/2016/10/09090408/toyota_logo_o.jpg"
+    },
+    {
+        type: "toyota",
+        startingYear: "1937",
+        image: "https://cdn-ds.com/blogs-media/sites/264/2016/10/09090408/toyota_logo_o.jpg"
+    },
+    {
+        type: "toyota",
+        startingYear: "1937",
+        image: "https://cdn-ds.com/blogs-media/sites/264/2016/10/09090408/toyota_logo_o.jpg"
+    },
+    {
+        type: "toyota",
+        startingYear: "1937",
+        image: "https://cdn-ds.com/blogs-media/sites/264/2016/10/09090408/toyota_logo_o.jpg"
+    },
+    {
+        type: "toyota",
+        startingYear: "1937",
+        image: "https://cdn-ds.com/blogs-media/sites/264/2016/10/09090408/toyota_logo_o.jpg"
+    },
+    {
+        type: "toyota",
+        startingYear: "1937",
+        image: "https://cdn-ds.com/blogs-media/sites/264/2016/10/09090408/toyota_logo_o.jpg"
+    },
+    {
+        type: "toyota",
+        startingYear: "1937",
+        image: "https://cdn-ds.com/blogs-media/sites/264/2016/10/09090408/toyota_logo_o.jpg"
+    },
+    {
+        type: "toyota",
+        startingYear: "1937",
+        image: "https://cdn-ds.com/blogs-media/sites/264/2016/10/09090408/toyota_logo_o.jpg"
+    },
+    {
+        type: "toyota",
+        startingYear: "1937",
+        image: "https://cdn-ds.com/blogs-media/sites/264/2016/10/09090408/toyota_logo_o.jpg"
+    },
+    {
+        type: "toyota",
+        startingYear: "1937",
+        image: "https://cdn-ds.com/blogs-media/sites/264/2016/10/09090408/toyota_logo_o.jpg"
+    },
+    {
+        type: "toyota",
+        startingYear: "1937",
+        image: "https://cdn-ds.com/blogs-media/sites/264/2016/10/09090408/toyota_logo_o.jpg"
+    },
+    {
+        type: "toyota",
+        startingYear: "1937",
+        image: "https://cdn-ds.com/blogs-media/sites/264/2016/10/09090408/toyota_logo_o.jpg"
+    },
+    {
+        type: "toyota",
+        startingYear: "1937",
+        image: "https://cdn-ds.com/blogs-media/sites/264/2016/10/09090408/toyota_logo_o.jpg"
+    },
+    {
+        type: "toyota",
+        startingYear: "1937",
+        image: "https://cdn-ds.com/blogs-media/sites/264/2016/10/09090408/toyota_logo_o.jpg"
+    },
+    {
+        type: "toyota",
+        startingYear: "1937",
+        image: "https://cdn-ds.com/blogs-media/sites/264/2016/10/09090408/toyota_logo_o.jpg"
+    }
+]
