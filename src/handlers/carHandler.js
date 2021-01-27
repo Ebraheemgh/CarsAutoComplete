fe00ef94c541cfaed9ed9bdb14c47dcca008a5bc
Haneen Awad <jamiecoe66@gmail.com>
2021-01-27 16:52:40 +0200

    last version

--- src/handlers/carHandler.js
+++ src/handlers/carHandler.js
@@ -1,38 +1,34 @@
 const fetchCar = require("../fetchData");
 
 function carHandler(request, response) {
-    let body = "";
-    // console.log((new URL(`http://${request.url}`)).searchParams.get("q"));
+  let body = "";
 
-    body = (new URL(`http://${request.url}`)).searchParams.get("q");
+  body = new URL(`http://${request.url}`).searchParams.get("q");
 
+  let carsList = [];
+  fetchCar()
+    .then((data) => {
+      carsList = [...data];
 
+      let dataToReturn = [];
+      for (let i = 0; i < carsList.length; i++) {
+        if (
+          carsList[i].make.includes(body) ||
+          carsList[i].model.includes(body)
+        ) {
+          dataToReturn.push(carsList[i]);
+        }
+      }
 
-    let carsList = [];
-    fetchCar()
-        .then(data => {
-            carsList = [...data]
-
-            let dataToReturn = [];
-            for (let i = 0; i < carsList.length; i++) {
-                if (carsList[i].make.includes(body) || carsList[i].model.includes(body)) {
-                    dataToReturn.push(carsList[i]);
-                }
-            }
-
-
-            if (dataToReturn.length > 0) {
-                response.writeHead(200, { "content-type": "application/json" });
-                response.end(JSON.stringify(dataToReturn));
-            } else {
-                response.writeHead(404, { "content-type": "application/json" });
-                response.end(JSON.stringify(dataToReturn));
-
-            }
-
-        }).catch(error => console.error(error))
-
-
+      if (dataToReturn.length > 0) {
+        response.writeHead(200, { "content-type": "application/json" });
+        response.end(JSON.stringify(dataToReturn));
+      } else {
+        response.writeHead(404, { "content-type": "application/json" });
+        response.end(JSON.stringify(dataToReturn));
+      }
+    })
+    .catch((error) => console.error(error));
 }
 
 module.exports = carHandler;
--- src/public/app.js
+++ src/public/app.js
@@ -1,61 +1,67 @@
 function getCars(body) {
-    let apiurl = "https://cars-auto-complete.herokuapp.com/";
-    const suggestion = document.getElementById("suggestion");
-    const output = document.getElementById("output");
-    output.style.display = "none";
-    suggestion.innerHTML = "";
+  let apiurl = "https://cars-auto-complete.herokuapp.com/";
+  const suggestion = document.getElementById("suggestion");
+  const output = document.getElementById("output");
+  output.style.display = "none";
+  suggestion.innerHTML = "";
 
-    if (body == "") {
-        return;
-    }
-    fetch(`${apiurl}car?q=${body}`, {
-            method: "GET"
-        })
-        .then(response => {
-            if (!response.ok) throw new Error(response.status);
+  if (body == "") {
+    return;
+  }
+  fetch(`${apiurl}car?q=${body}`, {
+    method: "GET",
+  })
+    .then((response) => {
+      if (!response.ok) throw new Error(response.status);
 
-            return response.json();
-        })
-        // if we get a successful response
-        .then(data => {
-            for (let i = 0; i < data.length; i++) {
-                const span = document.createElement("span");
-                span.textContent = data[i].make + " " + data[i].model;
-                span.addEventListener("click", event => {
-                    document.getElementById("carImg").src = data[i].img_url;
-                    document.getElementById("make").textContent = "Make= >" + data[i].make;
-                    document.getElementById("model").textContent = "Model= >" + data[i].model;
-                    document.getElementById("year").textContent = "Year= >" + data[i].year;
-                    document.getElementById("horsepower").textContent = "HorsePower=> " + data[i].horsepower;
-                    document.getElementById("price").textContent = "Price=> " + data[i].price + "$";
-                    output.style.display = "flex";
-                    suggestion.innerHTML = "";
-
-                })
-                suggestion.appendChild(span)
-            }
-
-        })
-        .catch(error => {
-            if (error.message === "404") {
-                suggestion.innerHTML = "";
-                const span = document.createElement("span");
-                span.textContent = "Caar Not found";
-                suggestion.appendChild(span)
-            }
+      return response.json();
+    })
+    // if we get a successful response
+    .then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        const span = document.createElement("span");
+        const img = document.createElement("img");
+        img.src = data[i].img_url;
+        span.appendChild(img);
+        const t = document.createTextNode(data[i].make + " " + data[i].model);
+        span.appendChild(t);
+        span.addEventListener("click", (event) => {
+          document.getElementById("carImg").src = data[i].img_url;
+          document.getElementById("make").textContent =
+            "Make= >" + data[i].make;
+          document.getElementById("model").textContent =
+            "Model= >" + data[i].model;
+          document.getElementById("year").textContent =
+            "Year= >" + data[i].year;
+          document.getElementById("horsepower").textContent =
+            "HorsePower=> " + data[i].horsepower;
+          document.getElementById("price").textContent =
+            "Price=> " + data[i].price + "$";
+          output.style.display = "flex";
+          suggestion.innerHTML = "";
         });
+        suggestion.appendChild(span);
+      }
+    })
+    .catch((error) => {
+      if (error.message === "404") {
+        suggestion.innerHTML = "";
+        const span = document.createElement("span");
+        span.textContent = "Caar Not found";
+        suggestion.appendChild(span);
+      }
+    });
 }
 let time = 500;
 let timeOut;
-document.getElementById("carInput").addEventListener("keyup", event => {
-    const input = document.getElementById("carInput").value;
-    clearTimeout(timeOut);
-    timeOut = setTimeout(() => {
-        getCars(input);
-    }, time);
+document.getElementById("carInput").addEventListener("keyup", (event) => {
+  const input = document.getElementById("carInput").value;
+  clearTimeout(timeOut);
+  timeOut = setTimeout(() => {
+    getCars(input);
+  }, time);
+});
 
-})
-
-document.querySelector("form").addEventListener("submit", event => {
-    event.preventDefault();
-})
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+});
