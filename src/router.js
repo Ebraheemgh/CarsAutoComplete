8c6ad0603a521be2f13b7a0113952c8756053c2b
mohamadsawaed <mohamad.sawaed.22@gmail.com>
2021-02-09 14:52:42 +0200

    loginhandler file

--- src/router.js
+++ src/router.js
@@ -3,18 +3,21 @@ const homeHandlers = require("./handlers/homeHandlers");
 const missingHandler = require("./handlers/missingHandler");
 const publicHandler = require("./handlers/publicHandler");
 const googleHandler = require("./handlers/imageGoogleSearch");
+const loginHandler = require("./handlers/loginHandler");
 const router = (request, response) => {
-    const url = request.url;
-    if (url === "/") {
-        homeHandlers(request, response);
-    } else if (url.includes("public")) {
-        publicHandler(request, response);
-    } else if (url.includes("car")) {
-        carHandler(request, response);
-    } else if (url.includes("google")) {
-        googleHandler(request, response);
-    } else {
-        missingHandler(request, response);
-    }
+  const url = request.url;
+  if (url === "/") {
+    homeHandlers(request, response);
+  } else if (url.includes("public")) {
+    publicHandler(request, response);
+  } else if (url.includes("car")) {
+    carHandler(request, response);
+  } else if (url.includes("google")) {
+    googleHandler(request, response);
+  } else if (url.includes("login")) {
+    loginHandler(request, response);
+  } else {
+    missingHandler(request, response);
+  }
 };
 module.exports = router;
